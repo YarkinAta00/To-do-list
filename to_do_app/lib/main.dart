18d96581384a5a1aed9c8621bd4b1f6f6bbfcec6
yarkinata <yarkinata6268@gmail.com>
2023-03-05 01:01:55 +0300

    Second Version

--- to_do_app/lib/main.dart
+++ to_do_app/lib/main.dart
@@ -7,90 +7,128 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'To-do App',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'To-do App'),
+      home: const MyHomePage(),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-  final String title;
+  const MyHomePage({super.key});
 
   @override
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-
-  final TextEditingController textController= TextEditingController();
-  List tasks=[];
+  final TextEditingController titleController = TextEditingController();
+  final TextEditingController tasksController = TextEditingController();
+  List taskTitles = [];
+  List tasks = [];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-       elevation: 0,
-       leading: const Icon(Icons.list_alt),
-        title: Text(widget.title),
-      ),
-      body:Column(
-        children: [
-          const SizedBox(height: 20),
-          const Text("To-Do App",style: TextStyle(fontSize: 25),),
-          Padding(
-            padding: const EdgeInsets.all(10.0),
-            child: Row(
+        appBar: AppBar(
+          elevation: 0,
+          leading: const Icon(Icons.list_alt),
+        ),
+        body: Column(
+          children: [
+            const SizedBox(height: 20),
+            const Text(
+              "To-Do App",
+              style: TextStyle(fontSize: 28),
+            ),
+            const SizedBox(height: 20),
+            Row(
               crossAxisAlignment: CrossAxisAlignment.center,
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
                 Expanded(
-                  child: TextFormField(
-                    controller: textController,
-                    decoration:InputDecoration(
-                      hintText: "Enter your job here...",
-                      label: const Text("To-do"),
-                      labelStyle: const TextStyle(fontSize: 20),
-                      helperText: "Enter your job here...",
-                      border: OutlineInputBorder(borderRadius: BorderRadius.circular(10)),
+                  child: Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 20),
+                    child: TextFormField(
+                      controller: titleController,
+                      decoration: InputDecoration(
+                        label: const Text("Task Title"),
+                        labelStyle: const TextStyle(fontSize: 20),
+                        helperText: "Enter your task title ",
+                        border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(10)),
+                      ),
                     ),
                   ),
                 ),
-                IconButton(onPressed:add, icon: const Icon(Icons.add,size: 50,)),
               ],
             ),
-          ),
-          Expanded(
-            child: ListView.builder(
-             padding: const EdgeInsets.all(10),
-              physics: const BouncingScrollPhysics(),
-              shrinkWrap: true,
-              itemCount: tasks.length,
-              itemBuilder: (BuildContext context,int index){
-                return Card(
-                   shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
-                 elevation: 10,
-                  child: ListTile(
-                    title: const Text("Task",style: TextStyle(fontSize: 25),),
-                    subtitle: Text(tasks[index],style: const TextStyle(fontSize: 22)),
-                              ),
-                );
-              } 
+            const SizedBox(height: 15),
+            Row(
+              children: [
+                Expanded(
+                  child: Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 20),
+                    child: TextFormField(
+                      controller: tasksController,
+                      decoration: InputDecoration(
+                        label: const Text("Task Detail"),
+                        labelStyle: const TextStyle(fontSize: 20),
+                        helperText: "Enter your task detail",
+                        border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(10)),
+                      ),
+                    ),
+                  ),
                 ),
-          )],
-      )
-    );
+              ],
+            ),
+            SizedBox(
+              child: IconButton(
+                  onPressed: add,
+                  icon: const Icon(
+                    Icons.check,
+                    size: 50,
+                    color: Colors.green,
+                  )),
+            ),
+            const SizedBox(height: 20),
+            Expanded(
+              child: ListView.builder(
+                  padding: const EdgeInsets.all(10),
+                  physics: const BouncingScrollPhysics(),
+                  shrinkWrap: true,
+                  itemCount: tasks.length,
+                  itemBuilder: (BuildContext context, int index) {
+                    return Card(
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(10)),
+                      elevation: 10,
+                      child: ListTile(
+                        title: Text(
+                          taskTitles[index],
+                          style: const TextStyle(fontSize: 25),
+                        ),
+                        subtitle: Text(tasks[index],
+                            style: const TextStyle(fontSize: 22)),
+                      ),
+                    );
+                  }),
+            ),
+          ],
+        ));
   }
-  void add(){
+
+  void add() {
     setState(() {
-      tasks.add(textController.text);
-      textController.clear();
+      taskTitles.add(titleController.text);
+      titleController.clear();
+
+      tasks.add(tasksController.text);
+      tasksController.clear();
     });
   }
 }
